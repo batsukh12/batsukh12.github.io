bbcb97c80975ccf233c91bdd0181bc399dc7900b
batsukh12 <112690266+batsukh12@users.noreply.github.com>
2022-11-15 10:52:23 +0800

    css2

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 
+
+$(document).ready(function() {
+    $('#menu-btn').click(function() {
+        $(this).toggleClass('fa fa-times');
+        $('.mid-menu').toggleClass('nav-toggle');
+    });
+});
