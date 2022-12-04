b87cbdc1c30c122e05758a546b8898ad49b959aa
batsukh12 <112690266+batsukh12@users.noreply.github.com>
2022-12-04 13:52:08 +0800

    css-ywts

--- script.js
+++ script.js
@@ -1,8 +1,27 @@
 
-
 $(document).ready(function() {
     $('#menu-btn').click(function() {
         $(this).toggleClass('fa fa-times');
         $('.mid-menu').toggleClass('nav-toggle');
     });
 });
+/** buteegdehuun nemeh func */
+var click=0;
+function plus(){
+    click++;
+    document.getElementById('count').innerHTML= click;
+    return click;
+};
+function minus(){
+    if(click==0){
+        alert("aldaa!!");
+        document.getElementById('count').innerHTML= 0;
+    } else{
+        click--;
+        document.getElementById('count').innerHTML= click;
+    }
+}
+
+function listChange(){
+    document.getElementById('lists').style.cssText = 'border-bottom: 2px solid #fff';
+}
