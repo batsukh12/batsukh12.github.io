e19ed3d7ae420df7187607dceb1eeaaffbac426e
Ankhbayarbt <112833093+Ankhbayarbt@users.noreply.github.com>
2022-12-06 10:12:08 +0800

    ,,,,

--- script.js
+++ script.js
@@ -1,8 +1,35 @@
 
-
+/** menu bar  */
 $(document).ready(function() {
     $('#menu-btn').click(function() {
         $(this).toggleClass('fa fa-times');
         $('.mid-menu').toggleClass('nav-toggle');
     });
 });
+/** buteegdehuun nemeh func */
+var click=0;
+function plus(){
+    click++;
+    document.getElementById('count').innerHTML= click;
+    return click;
+};
+function minus(){
+    if(click==0){
+        alert("aldaa!!");
+        document.getElementById('count').innerHTML= 0;
+    } else{
+        click--;
+        document.getElementById('count').innerHTML= click;
+    }
+}
+
+function listChange(){
+    document.getElementById('lists').style.cssText = 'border-bottom: 2px solid #fff';
+}
+/***   shop bot sagsalah too */
+var count=0;
+function shopping(){
+    count++;
+    document.getElementById('shop-count').innerHTML=count;
+    return count;
+}
